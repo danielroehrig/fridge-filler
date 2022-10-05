4c31eb529048c6339eca08b77623dab1c1c95f95
Daniel Röhrig <mail@danielroehrig.de>
2022-10-05 23:01:32 +0200

    Update docs
    Signed-off-by: Daniel Röhrig <mail@danielroehrig.de>

--- lib/provider/database_provider.dart
+++ lib/provider/database_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:multi_shop_list/models/list_model.dart';
 
+/// A provider for accessing the hive key value store aka the database
 class DatabaseProvider extends InheritedWidget {
   DatabaseProvider({required super.child}) : super();
   final Future<Box<ListEntry>> _box = Hive.openBox<ListEntry>('box');
@@ -9,6 +10,7 @@ class DatabaseProvider extends InheritedWidget {
   static DatabaseProvider? of(BuildContext context) =>
       context.dependOnInheritedWidgetOfExactType<DatabaseProvider>();
 
+  /// Adds a new list add the bottom of the list of lists
   Future<void> addList(String listName) {
     return _box.then((box) {
       ListEntry listEntry = ListEntry(name: listName);
@@ -16,6 +18,7 @@ class DatabaseProvider extends InheritedWidget {
     });
   }
 
+  /// Gets all lists in the order they were saved
   Future<List<ListEntry>> getLists() {
     return _box.then((box) {
       return box.values.toList();
