ce1b9837bab63166829f2edc50b9a0aee11cc799
Daniel Röhrig <mail@danielroehrig.de>
2023-01-26 22:18:25 +0100

    Get shared preferences into the database provider
    Signed-off-by: Daniel Röhrig <mail@danielroehrig.de>

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,16 @@ import 'package:fridge_filler/models/list_model.dart';
 import 'package:fridge_filler/pages/home_page.dart';
 import 'package:fridge_filler/provider/database_provider.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   await Hive.initFlutter('hive_boxes');
   //Hive.openLazyBox<ListEntry>('box').then((box) => box.deleteFromDisk());
   Hive.registerAdapter(ItemEntryAdapter());
   Hive.registerAdapter(ListEntryAdapter());
+  final prefs = await SharedPreferences.getInstance();
   var box = await Hive.openBox<ListEntry>('box');
-  runApp(DatabaseProvider(box: box, child: const FridgeFillerApp()));
+  runApp(DatabaseProvider(box: box, prefs: prefs, child: const FridgeFillerApp()));
 }
 
 class FridgeFillerApp extends StatelessWidget {
